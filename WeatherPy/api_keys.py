01aa33d04ce454b8ee3fce684d9f1337f2ea2804
alabidj <alabi_dj@yahoo.com>
2024-11-30 09:52:31 -0500

    plus API keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "11e7f4ca9ee272e343cfaabb7a799e5f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d2b2a5b9cffc419eb3b1782aa2866be0"
